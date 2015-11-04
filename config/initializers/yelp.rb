b1f53f0c9a29b29cedb7d5a85fde128077d5a373
chenghw <ho.won.cheng@gmail.com>
2015-11-04 15:41:33 -0500

    edits to yelp keys

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -12,10 +12,10 @@ require 'pry'
   # end
 
   Yelp.client.configure do |config|
-    config.consumer_key = "G2XLqW5t9SaRkb5Ubo0yHg"
-    config.consumer_secret = "FG6Kz2YB5e1qPnZRs9VXvyPeMcM"
-    config.token = "cjmpFB_YjZ1x3vZA6DMPji6ITw9ZdKMt"
-    config.token_secret = "eCqO1y3X2jnum0LPow5Yrtso7lM"
+    config.consumer_key = ""
+    config.consumer_secret = ""
+    config.token = ""
+    config.token_secret = ""
   end
   # binding.pry
 
